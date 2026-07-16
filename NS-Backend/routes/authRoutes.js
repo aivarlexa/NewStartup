17acd508d83b5a56b12c273ff8d5851da8b3b7f3
Prachi Nadgir <nadgirprachi@gmail.com>
2026-07-16 18:45:46 +0530

    feat: add admin login route to authentication

--- NS-Backend/routes/authRoutes.js
+++ NS-Backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { register, login, forgotPassword, googleLogin } = require("../controller/authController");
+const { register, login,adminLogin, forgotPassword, googleLogin } = require("../controller/authController");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/admin/login", adminLogin);
 router.post("/forgot-password", forgotPassword);
 router.post("/google", googleLogin);
 
